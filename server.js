417e5d414a79c9b0a928ca57351ddde917b46530
Jan Kaßel <jan@kassel.works>
2019-11-14 23:24:16 +0100

    draft events processing server

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const { connectToCouch, insertEntry } = require('./lib/couch')
+const { validateEvent } = require('./lib/event')
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
-});
+if (process.env.NOD_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-const server = app.listen(process.env.PORT || 3000, () =>
-  process.stdout.write(`Server listening on port ${server.address().port}.\n`)
-);
+const requiredArgs = ['COUCHDB_URL', 'COUCHDB_DATABASE']
+
+async function main () {
+  if (!requiredArgs.every(arg => !!process.env[arg])) {
+    console.error(`Some required environment arguments weren't set:
+${JSON.stringify(requiredArgs, null, 2)}`)
+    process.exit(1)
+  }
+
+  await connectToCouch(process.env.COUCHDB_URL, process.env.COUCHDB_DATABASE)
+
+  const app = express()
+  app.use(express.json())
+
+  app.post('/event', async (req, res) => {
+    const event = req.body
+    if (!validateEvent(event)) {
+      res.status(400).send('bad request')
+      return
+    }
+
+    try {
+      await insertEntry(event)
+      res.status(200).send('ok')
+    } catch (err) {
+      console.error('Unexpected error:', err)
+      res.status(500).send('internal server error')
+    }
+  })
+
+  const server = app.listen(process.env.PORT || 3000, () =>
+    process.stdout.write(`Server listening on port ${server.address().port}.\n`)
+  )
+}
+main()
